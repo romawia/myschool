a4ba7e09daeeeeea038a2461cbdc41a842a19f82
romawia <68232092+romawia@users.noreply.github.com>
2020-07-20 21:22:07 +0530

    replacement of old files

--- indexjava.js
+++ indexjava.js
@@ -4,7 +4,7 @@ function valid()
 
 var user = document.getElementById("logname").value;
 var passw= document.getElementById("logpass");
-var classzat = document.getElementById("class").value;
+
 var exact= "sertehighschool";
 
 if(passw.value!==exact)
